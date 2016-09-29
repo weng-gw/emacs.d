f31161654f6c9f741ab008438df08028bf82d206
wengx076 <wengx076@umn.edu>
2016-09-29 18:52:57 -0500

    Delete better-defaults and Add ESS AUCTEX configuration

--- init.el
+++ init.el
@@ -4,12 +4,13 @@
 ;;--------------------------------------------
 
 (require 'package)
-(setq package-list '(better-defaults
-					  material-theme
+(setq package-list '( material-theme
 					  ein
 					  elpy
 					  flycheck
-					  py-autopep8))
+					  py-autopep8
+					  ess
+					  auctex))
 
 
 (add-to-list 'package-archives
@@ -27,12 +28,16 @@
 
 ;; BASIC CUSTOMIZATION
 ;;----------------------------------------------------
-
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq column-number-mode t)
 (setq inhibit-startup-message t) ;; hide the startup message
 (load-theme 'material t) ;; load material theme
 (global-linum-mode t) ;; enable line numbers globally
-
-
+(set-default-font "Monospace-16") ;; set font and font size
+(setq-default auto-fill-function 'do-auto-fill) ;; enable auto-fill globally
+(fset 'yes-or-no-p 'y-or-n-p)
 ;;PYTHON CONFIGURATION
 ;;-----------------------------------------------------
 
@@ -49,4 +54,21 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
+
+;; ESS CONFIGURATION
+;;-----------------------------------------------------
+(require 'ess-site)
+
+;; AUCTEX CONFIGURATION
+;;-----------------------------------------------------
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+(setq TeX-output-view-style (quote (("^pdf$" "." "evince %o %(outpage)"))))
+(add-hook 'LaTeX-mode-hook
+		  (lambda()
+			(latex-math-mode 1)
+			(add-to-list
+			  'TeX-command-list' ("XeLaTeX" "%`xelatex%(mode)%' %t" Tex-run-Tex nil t))
+			(setq Tex-command-default "XeLaTeX")
+			(setq Tex-show-compilation t)))
 ;; init.el ends here
