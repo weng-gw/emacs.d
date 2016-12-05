17507228823a3d32a2dddf5a07c4d670f158f2d0
wengx076 <wengx076@umn.edu>
2016-12-05 14:51:40 -0600

    Change ESS sweaver to knitr

--- init.el
+++ init.el
@@ -95,15 +95,3 @@
 (require 'cc-conf)
 (require 'fortran-conf)
 ;; init.el ends here
-;(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-; '(ess-swv-processor (quote knitr)))
-;(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;)
