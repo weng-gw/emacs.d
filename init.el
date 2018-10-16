758712ffd7a6ca394805727ebaecc292a5a9a56d
Guangwei Weng <wgw@MacBook.local>
2018-10-16 16:37:43 -0500

    delete auto set variable

--- init.el
+++ init.el
@@ -99,17 +99,3 @@
 
 (use-package magit
   :bind ("C-c g" . magit-status))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auto-complete window-numbering use-package smart-mode-line miniedit material-theme magit hc-zenburn-theme exec-path-from-shell ess auto-compile auctex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
