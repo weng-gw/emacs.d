8ffcb38ec6af7cb40f5886c11144a39e9e742ab4
weng-gw <weng.gw@outlook.com>
2018-10-17 16:27:20 -0500

    add basic config for ein

--- init.el
+++ init.el
@@ -76,40 +76,19 @@
   :init (ac-config-default))
 
 ;; auctex setting for MacOS with Skim
-(use-package auctex
-  :hook (latex-mode . flyspell-mode)
-  :init
-  (setq TeX-PDF-mode t)
-  ;;(setq Tex-output-view-style (quote (("^pdf$" "." "open %o %(outpage%)"))))
-  (setq TeX-view-program-selection '((output-pdf "Skim")))
-  (setq TeX-view-program-list
-	'(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
-  (add-hook 'LaTeX-mode-hook
-	    (lambda()
-	      (latex-math-mode 1)
-	      (add-to-list
-	       'TeX-command-list '("XeLaTeX" "%`xelatex -synctex=1%(mode)%' %t" TeX-run-TeX nil t))
-	      (setq TeX-command-default "XeLaTeX")
-	      (setq TeX-show-compilation nil)))
-  (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-  (setq TeX-source-correlate-method 'synctex)
-  (setq TeX-source-correlate-mode t)
-  (setq TeX-source-correlate-start-server t)
-  )
-
-;; auctex setting for Linux with evince
 ;; (use-package auctex
 ;;   :hook (latex-mode . flyspell-mode)
 ;;   :init
 ;;   (setq TeX-PDF-mode t)
 ;;   ;;(setq Tex-output-view-style (quote (("^pdf$" "." "open %o %(outpage%)"))))
-;;   (setq TeX-output-view-style (quote (("^pdf$" "." "evince %o %(outpage)"))))
+;;   (setq TeX-view-program-selection '((output-pdf "Skim")))
+;;   (setq TeX-view-program-list
+;; 	'(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
 ;;   (add-hook 'LaTeX-mode-hook
 ;; 	    (lambda()
 ;; 	      (latex-math-mode 1)
 ;; 	      (add-to-list
-;; 	       'TeX-command-list' ("XeLaTeX" "%`xelatex -synctex=1%(mode)%' %t" TeX-run-TeX nil t))
+;; 	       'TeX-command-list '("XeLaTeX" "%`xelatex -synctex=1%(mode)%' %t" TeX-run-TeX nil t))
 ;; 	      (setq TeX-command-default "XeLaTeX")
 ;; 	      (setq TeX-show-compilation nil)))
 ;;   (add-hook 'LaTeX-mode-hook 'visual-line-mode)
@@ -118,6 +97,27 @@
 ;;   (setq TeX-source-correlate-mode t)
 ;;   (setq TeX-source-correlate-start-server t)
 ;;   )
+
+;; auctex setting for Linux with evince
+(use-package auctex
+  :hook (latex-mode . flyspell-mode)
+  :init
+  (setq TeX-PDF-mode t)
+  ;;(setq Tex-output-view-style (quote (("^pdf$" "." "open %o %(outpage%)"))))
+  (setq TeX-output-view-style (quote (("^pdf$" "." "evince %o %(outpage)"))))
+  (add-hook 'LaTeX-mode-hook
+	    (lambda()
+	      (latex-math-mode 1)
+	      (add-to-list
+	       'TeX-command-list' ("XeLaTeX" "%`xelatex -synctex=1%(mode)%' %t" TeX-run-TeX nil t))
+	      (setq TeX-command-default "XeLaTeX")
+	      (setq TeX-show-compilation nil)))
+  (add-hook 'LaTeX-mode-hook 'visual-line-mode)
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  (setq TeX-source-correlate-method 'synctex)
+  (setq TeX-source-correlate-mode t)
+  (setq TeX-source-correlate-start-server t)
+  )
 
 
 
@@ -135,4 +135,12 @@
 	 (setq elpy-rpc-backend "jedi"))
 
 
+(use-package ein
+  :init (require 'ein)
+  (require 'ein-loaddefs)
+  (require 'ein-notebook)
+  (require 'ein-subpackages)
+  )
+
+	   
 
