320591f7e634e8084494dde67196cb4d1c7fa365
weng-gw <weng.gw@outlook.com>
2018-10-24 22:02:41 -0500

    fix lazy load for elpy

--- init.el
+++ init.el
@@ -105,22 +105,20 @@
   :init
   (setq TeX-PDF-mode t)
   ;;(setq Tex-output-view-style (quote (("^pdf$" "." "open %o %(outpage%)"))))
-  (setq TeX-view-program-selection '((output-pdf "Skim")))
-  (setq TeX-view-program-list
-	'(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+  (setq TeX-output-view-style (quote (("^pdf$" "." "evince %o %(outpage)"))))
   (add-hook 'LaTeX-mode-hook
 	    (lambda()
 	      (latex-math-mode 1)
 	      (add-to-list
-	       'TeX-command-list '("XeLaTeX" "%`xelatex -synctex=1%(mode)%' %t" TeX-run-TeX nil t))
+	       'TeX-command-list' ("XeLaTeX" "%`xelatex -synctex=1%(mode)%' %t" TeX-run-TeX nil t))
 	      (setq TeX-command-default "XeLaTeX")
 	      (setq TeX-show-compilation nil)))
   (add-hook 'LaTeX-mode-hook 'visual-line-mode)
   (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
   (setq TeX-source-correlate-method 'synctex)
   (setq TeX-source-correlate-mode t)
-  (setq TeX-source-correlate-start-server t)
-  )
+  (setq TeX-source-correlate-start-server t))
+
 
 
 
@@ -133,7 +131,7 @@
 
 
 (use-package elpy
-  :defer t
+  :after python  
   :config (elpy-enable)
   (setq python-shell-interpreter "jupyter"
 	python-shell-interpreter-args "console  --simple-prompt"
