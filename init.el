5c733d32270a066777412f4ff697fd1dce0cc8b8
Guangwei Weng <wgw@MacBook.local>
2018-11-30 22:36:52 -0600

    merge from linux setup, create new branch for mac. will maintain two branch separately

--- init.el
+++ init.el
@@ -76,9 +76,35 @@
   :config (ac-config-default))
 
 ;; auctex setting for MacOS with Skim
+(use-package auctex
+  :hook  (LaTeX-mode . flyspell-mode)
+  :init
+  (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+  (setq TeX-PDF-mode t)
+  (setq TeX-view-program-selection '((output-pdf "Skim")))
+  (setq TeX-view-program-list
+	'(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+  (add-hook 'LaTeX-mode-hook
+	    (lambda()
+	      (latex-math-mode 1)
+	      (add-to-list
+	       'TeX-command-list' ("XeLaTeX" "%`xelatex -synctex=1%(mode)%' %t" TeX-run-TeX nil t))
+	      (setq TeX-command-default "XeLaTeX")
+	      (setq TeX-show-compilation nil)))
+  (add-hook 'LaTeX-mode-hook 'visual-line-mode)
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  (setq TeX-source-correlate-method 'synctex)
+  (setq TeX-source-correlate-mode t)
+  (setq TeX-source-correlate-start-server t)
+  )
+
+
+
+;; auctex setting for Linux with evince
 ;; (use-package auctex
-;;   :hook  (LaTeX-mode . flyspell-mode)
+;;   :hook (LaTeX-mode . flyspell-mode)
 ;;   :init
+;;   (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
 ;;   (setq TeX-PDF-mode t)
 ;;   (setq TeX-output-view-style (quote (("^pdf$" "." "evince %o %(outpage)"))))
 ;;   (add-hook 'LaTeX-mode-hook
@@ -92,30 +118,7 @@
 ;;   (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 ;;   (setq TeX-source-correlate-method 'synctex)
 ;;   (setq TeX-source-correlate-mode t)
-;;   (setq TeX-source-correlate-start-server t)
-;;   )
-
-
-
-;; auctex setting for Linux with evince
-(use-package auctex
-  :hook (LaTeX-mode . flyspell-mode)
-  :init
-  (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
-  (setq TeX-PDF-mode t)
-  (setq TeX-output-view-style (quote (("^pdf$" "." "evince %o %(outpage)"))))
-  (add-hook 'LaTeX-mode-hook
-	    (lambda()
-	      (latex-math-mode 1)
-	      (add-to-list
-	       'TeX-command-list' ("XeLaTeX" "%`xelatex -synctex=1%(mode)%' %t" TeX-run-TeX nil t))
-	      (setq TeX-command-default "XeLaTeX")
-	      (setq TeX-show-compilation nil)))
-  (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-  (setq TeX-source-correlate-method 'synctex)
-  (setq TeX-source-correlate-mode t)
-  (setq TeX-source-correlate-start-server t))
+;;   (setq TeX-source-correlate-start-server t))
 
 
 (use-package magit
