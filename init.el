5f3a76cad8d154a0ceb8a4373d4508f58399fb29
Guangwei Weng <wgw@MacBook.local>
2020-07-26 21:41:16 -0500

    regular update

--- init.el
+++ init.el
@@ -51,9 +51,9 @@
 ;;(global- linum-mode t)
 (global-set-key (kbd "C-c l") 'goto-line)
 (setq inhibit-startup-message t)
-(set-frame-font "Monospace-13" t t)
-(add-to-list 'default-frame-alist '(font . "Monospace-13" ))
-(set-face-attribute 'default t :font "Monospace-13" )
+(set-frame-font "Menlo-13" t t)
+(add-to-list 'default-frame-alist '(font . "Menlo-13" ))
+(set-face-attribute 'default t :font "Menlo-13" )
 (show-paren-mode 1)
 (use-package smart-mode-line)
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -78,6 +78,12 @@
 
 (use-package hc-zenburn-theme
   :config (load-theme 'hc-zenburn t))
+
+;; (use-package color-theme-sanityinc-tomorrow
+;;   :config (color-theme-sanityinc-tomorrow-day))
+
+(use-package recentf
+  :init (recentf-mode 1))
 
 (use-package neotree
   :config (global-set-key (kbd "C-c t") 'neotree-toggle)
@@ -173,12 +179,47 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#000000" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#eaeaea"))
+ '(beacon-color "#d54e53")
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-safe-themes
+   (quote
+    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+ '(fci-rule-color "#424242")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(frame-background-mode (quote dark))
  '(package-selected-packages
    (quote
-    (uniquify window-numbering use-package smart-mode-line poly-R neotree miniedit material-theme magit htmlize hc-zenburn-theme exec-path-from-shell ess elpy ein auto-compile auctex))))
+    (color-theme-sanityinc-tomorrow window-numbering use-package smart-mode-line poly-R neotree miniedit material-theme magit htmlize hc-zenburn-theme exec-path-from-shell ess elpy ein auto-compile auctex)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#d54e53")
+     (40 . "#e78c45")
+     (60 . "#e7c547")
+     (80 . "#b9ca4a")
+     (100 . "#70c0b1")
+     (120 . "#7aa6da")
+     (140 . "#c397d8")
+     (160 . "#d54e53")
+     (180 . "#e78c45")
+     (200 . "#e7c547")
+     (220 . "#b9ca4a")
+     (240 . "#70c0b1")
+     (260 . "#7aa6da")
+     (280 . "#c397d8")
+     (300 . "#d54e53")
+     (320 . "#e78c45")
+     (340 . "#e7c547")
+     (360 . "#b9ca4a"))))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier" :foundry "nil" :slant normal :weight normal :height 130 :width normal)))))
+ '(default ((t (:family "Menlo" :foundry "nil" :slant normal :weight normal :height 130 :width normal)))))
--- templates/TEMPLATE.java.tpl
+++ templates/TEMPLATE.java.tpl
@@ -6,11 +6,10 @@
  * @author <a href="mailto:(>>>LOGIN_NAME<<<)@(>>>HOST_ADDR<<<)">(>>>USER_NAME<<<)</a>
  */
 
-public class (>>>FILE_SANS<<<)
-{
+public class (>>>FILE_SANS<<<){
   (>>>1<<<)
 }
->>>TEMPLATE-DEFINITION-SECTION<<<
-"Template defines 2 registers (message disappears with first user event):"
-(?m "public static void main(String[] argv)")
-(?p "System.out.println(")
+// >>>TEMPLATE-DEFINITION-SECTION<<<
+// "Template defines 2 registers (message disappears with first user event):"
+// (?m "public static void main(String[] argv)")
+// (?p "System.out.println(")
