b938f8c29628f945a20057013518d0ac2f1d980e
Guangwei Weng <wgw@Guangweis-MacBook-Pro.local>
2020-11-22 16:27:48 -0600

    remove bind of yasnippet to latex mode

--- init.el
+++ init.el
@@ -185,7 +185,7 @@
   :config (yas-reload-all)
   (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
   (add-hook 'python-mode 'yas-minor-mode)
-  (add-hook 'LaTeX-mode-hook 'yas-minor-mode)
+  ;;(add-hook 'LaTeX-mode-hook 'yas-minor-mode)
   (add-hook 'org-mode-hook 'yas-minor-mode)
   (add-hook 'scala-mode-hook 'yas-minor-mode))
 ;; note the snippets bundle needs to be installed separately
