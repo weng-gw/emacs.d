0d1321365f0d6f75861929f781548dcf07f919a5
Guangwei Weng <wgw@MacBookProApple1.local>
2021-07-26 14:23:42 -0500

    add configuration for treemacs and centaur-tabs, deprecate neotree

--- init.el
+++ init.el
@@ -85,9 +85,121 @@
 (use-package recentf
   :init (recentf-mode 1))
 
-(use-package neotree
-  :config (global-set-key (kbd "C-c t") 'neotree-toggle)
-  )
+;; (use-package neotree
+;;   :config (global-set-key (kbd "C-c t") 'neotree-toggle)
+;;   )
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-directory-name-transformer    #'identity
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-extension-regex          treemacs-last-period-regex-value
+          treemacs-file-follow-delay             0.2
+          treemacs-file-name-transformer         #'identity
+          treemacs-follow-after-init             t
+          treemacs-expand-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-move-forward-on-expand        nil
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-read-string-input             'from-child-frame
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-litter-directories            '("/node_modules" "/.venv" "/.cask")
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-asc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-user-mode-line-format         nil
+          treemacs-user-header-line-format       nil
+          treemacs-width                         35
+          treemacs-width-is-initially-locked     t
+          treemacs-workspace-switch-cleanup      nil)
+     ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+	;;change all C-x to C-c in order to not conflict template-mode
+        ("M-0"       . treemacs-select-window)
+        ("C-c t 1"   . treemacs-delete-other-windows)
+        ("C-c t t"   . treemacs)
+        ("C-c t B"   . treemacs-bookmark)
+        ("C-c t C-t" . treemacs-find-file)
+        ("C-c t M-t" . treemacs-find-tag)))
+
+;; (use-package treemacs-evil
+;;   :after (treemacs evil)
+;;   :ensure t)
+
+;; (use-package treemacs-projectile
+;;   :after (treemacs projectile)
+;;   :ensure t)
+
+(use-package treemacs-icons-dired
+  :after (treemacs dired)
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+;; (use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+;;   :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+;;   :ensure t
+;;   :config (treemacs-set-scope-type 'Perspectives))
+
+(use-package centaur-tabs
+  :demand
+  :config
+  (centaur-tabs-mode t)
+  (setq centaur-tabs-set-icons t)
+  (setq centaur-tabs-set-bar 'over)
+  (setq centaur-tabs-height 32)
+  (setq centaur-tabs-style "bar")
+  :bind
+  ("<M-left>" . centaur-tabs-backward)
+  ("<M-right>" . centaur-tabs-forward)
+  ("<M-up>" . centaur-tabs-backward-group)
+  ("<M-down>" . centaur-tabs-forward-group))
+
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
@@ -142,32 +254,17 @@
   :defer t
   :bind ("C-c g" . magit-status))
 
-(use-package elpy
-  :ensure t
-  :defer t
-  :init
-  (advice-add 'python-mode :before 'elpy-enable)
-  (add-hook 'python-mode-hook (lambda () (auto-complete-mode -1)))
-  ;;diable auto-complete-mode since it slows down editing and company is the default dependence
-  (pyvenv-activate (expand-file-name "~/opt/anaconda3/envs/py38"))
-  ;; and note that you need to install jupyter console for this enviromnet with 
-  ;; conda install -c anaconda jupyter_console
-  (auto-complete-mode -1)
-  (remove-hook 'elpy-modules 'elpy-module-flymake)
-  (setq python-shell-interpreter "jupyter"
-	python-shell-interpreter-args "console --simple-prompt"
-	       python-shell-prompt-detect-failure-warning nil)
-	 (add-to-list 'python-shell-completion-native-disabled-interpreters "jupyter")
-	 (setq elpy-rpc-backend "jedi")
-	 (setq elpy-shell-echo-output nil);; this command is used for fixing ^G problem in MacOS
-	 )
-
 ;; (use-package elpy
-;;   :after python  
-;;   :config (elpy-enable)
+;;   :ensure t
+;;   :defer t
+;;   :init
+;;   (advice-add 'python-mode :before 'elpy-enable)
+;;   (add-hook 'python-mode-hook (lambda () (auto-complete-mode -1)))
+;;   ;;diable auto-complete-mode since it slows down editing and company is the default dependence
 ;;   (pyvenv-activate (expand-file-name "~/opt/anaconda3/envs/py38"))
 ;;   ;; and note that you need to install jupyter console for this enviromnet with 
 ;;   ;; conda install -c anaconda jupyter_console
+;;   (auto-complete-mode -1)
 ;;   (remove-hook 'elpy-modules 'elpy-module-flymake)
 ;;   (setq python-shell-interpreter "jupyter"
 ;; 	python-shell-interpreter-args "console --simple-prompt"
@@ -177,16 +274,22 @@
 ;; 	 (setq elpy-shell-echo-output nil);; this command is used for fixing ^G problem in MacOS
 ;; 	 )
 
-;; (use-package conda
-;;   :defer t
-;;   :config (require 'conda)
-;;   (setq 
-;;    conda-env-home-directory (expand-file-name "~/opt/anaconda3/")
-;;    ))
-;; (custom-set-variables
-;;    '(conda-anaconda-home (expand-file-name "~/opt/anaconda3/")))
-
-;; (use-package anaconda-mode)
+(use-package elpy
+  :after python  
+  :config (elpy-enable)
+  (add-hook 'python-mode-hook (lambda () (auto-complete-mode -1)))
+  ;;diable auto-complete-mode since it slows down editing and company is the default dependence
+  (pyvenv-activate (expand-file-name "~/opt/anaconda3/envs/py38"))
+  ;; and note that you need to install jupyter console for this enviromnet with 
+  ;; conda install -c anaconda jupyter_console
+  (remove-hook 'elpy-modules 'elpy-module-flymake)
+  (setq python-shell-interpreter "jupyter"
+	python-shell-interpreter-args "console --simple-prompt"
+	       python-shell-prompt-detect-failure-warning nil)
+	 (add-to-list 'python-shell-completion-native-disabled-interpreters "jupyter")
+	 (setq elpy-rpc-backend "jedi")
+	 (setq elpy-shell-echo-output nil);; this command is used for fixing ^G problem in MacOS
+	 )
 
 (use-package ein
   :defer t
@@ -247,7 +350,6 @@
   ("scala" . scala-mode))
 
 
-
 (use-package yasnippet
   :config (yas-reload-all)
   (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
@@ -258,3 +360,20 @@
   (add-hook 'scala-mode-hook 'yas-minor-mode))
 ;; note the snippets bundle needs to be installed separately
 ;; use M-x package-list-packages to list all packages available and install yasnippet-snippets
+
+;; (use-package tabbar
+;;   :init (tabbar-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (all-the-icons yasnippet-snippets window-numbering use-package treemacs-magit treemacs-icons-dired tabbar smart-mode-line scala-mode poly-R neotree miniedit htmlize hc-zenburn-theme fold-this ess elpy ein conda centaur-tabs auto-complete auto-compile auctex anaconda-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:foreground "#DCDCCC" :background "#313131")))))
