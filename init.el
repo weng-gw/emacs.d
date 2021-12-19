a4c1a2f887d6fb7f5d0630912cca07d6e8e393cf
Guangwei Weng <wgw@Guangweis-MacBook-Pro.local>
2021-12-18 21:23:52 -0600

    Add new config of Python mode with anaconda-mode and eval-in-repl

--- init.el
+++ init.el
@@ -1,17 +1,20 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    General Settings
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (package-initialize)
 (setq usepackage-always-ensure t)
 
 (setq user-full-name "Guangwei Weng"
       user-mail-address "wengx076@umn.edu")
 
-;; (unless (assoc-default "melpa" package-archives)
-;;   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (unless (assoc-default "org" package-archives)
   (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t))
 
 
-(add-to-list 'load-path "~/.emacs.d/elisp")
+(add-to-list 'load-path "~/.emacs.d/elisp") ;; path for customized pacakges
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 (setq use-package-verbose t)
@@ -21,7 +24,6 @@
   :defer t
   :config (auto-compile-on-load-mode))
 (setq load-prefer-newer t)
-
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
@@ -44,16 +46,6 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (display-time-mode 1)
-
-;; (use-package smooth-scrolling
-;;   :ensure t
-;;   :init (require 'smooth-scrolling)
-;;   (smooth-scrolling-mode 1))
-
-;; (use-package good-scroll
-;;   :ensure t
-;;   :init (good-scroll-mode 1))
-
 
 (setq visible-bell 1)
 (setq column-number-mode t)
@@ -83,8 +75,7 @@
   :config (miniedit-install))
 (time-stamp)
 (add-hook 'write-file-hooks 'time-stamp)
-(require 'template)
-(template-initialize)
+
 
 (use-package hc-zenburn-theme
   :config (load-theme 'hc-zenburn t))
@@ -92,6 +83,8 @@
 ;; (use-package color-theme-sanityinc-tomorrow
 ;;   :config (color-theme-sanityinc-tomorrow-day))
 
+;; Recentf is a minor mode that builds a list of recently opened files.
+;; This list is is automatically saved across sessions on exiting Emacs
 (use-package recentf
   :init (recentf-mode 1))
 
@@ -116,13 +109,41 @@
   ("<s-up>" . centaur-tabs-backward-group)
   ("<s-down>" . centaur-tabs-forward-group))
 
-
+;; The library uniquify overrides Emacs’ default mechanism for making buffer names unique
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 (setq uniquify-separator "/")
 (setq uniquify-after-kill-buffer-p t)
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    Code templating
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; template was deprecated and will use snippets for future templates
+;; (require 'template)
+;; (template-initialize)
+
+(use-package yasnippet
+  :config (yas-reload-all)
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+  (add-hook 'python-mode 'yas-minor-mode)
+  ;;(add-hook 'LaTeX-mode-hook 'yas-minor-mode)
+  (add-hook 'org-mode-hook 'yas-minor-mode)
+  (add-hook 'markdown-mode-hook 'yas-minor-mode)
+  (add-hook 'scala-mode-hook 'yas-minor-mode)
+  (add-hook 'lisp-mode-hook 'yas-minor-mode))
+;; note the snippets bundle needs to be installed separately
+;; use M-x package-list-packages to list all packages available and install yasnippet-snippets or yasnippet-classic-snippets
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    Scientific Computing with R/ESS
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package ess
   :defer t
   :bind ("C-c C-s" . ess-switch-process)
@@ -131,6 +152,7 @@
   ;;(add-hook 'ess-mode-hook 'company-mode)
   )
 
+;; Use ploymode for R markdown
 (use-package polymode
   :defer t
   )
@@ -139,14 +161,11 @@
   :defer t
   )
 
-
-;; now deprecate auto-complete and try company-mode instead
-;;(use-package auto-complete)
-;;  :config (ac-config-default))
-
-;;(use-package company)
-
-;; auctex setting for MacOS with Skim
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    LaTex with auctex
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package auctex
   :hook  (LaTeX-mode . flyspell-mode)
   :init
@@ -171,10 +190,25 @@
 
 
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    Git with magit
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package magit
   :defer t
   :bind ("C-c g" . magit-status))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    Python configuration
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Desired features
+;; 1. Auto complete in Python script buffer
+;; 2. Send code line or region to buffer
 
 ;; (use-package python-mode
 ;;   :ensure t
@@ -185,10 +219,6 @@
   ;; 	python-shell-interpreter-args "-i"
   ;; 	python-shell-prompt-detect-failure-warning nil)
 ;;  )					
-
-;; (setq python-shell-interpreter "ipython"
-;;       python-shell-interpreter-args "-i"
-;;       python-shell-prompt-detect-failure-warning nil)
   
 ;; (use-package jupyter
 ;;   :ensure t
@@ -199,28 +229,112 @@
 ;;           '(unbind-key "C-c h" jupyter-org-interaction-mode-map)))  
 
 
-(use-package elpy
-  :ensure t
-  :after python  
-  :config (elpy-enable)
-  (setq elpy-rpc-backend "jedi")  
-  (setq company-idle-delay 0.5)
-  (setq elpy-company-add-completion-from-shell nil)
-  ;;(add-hook 'python-mode-hook (lambda () (auto-complete-mode -1)))
-  ;;diable auto-complete-mode since it slows down editing and company is the default dependence
-  (pyvenv-activate (expand-file-name "~/opt/anaconda3/envs/py38"))
-  ;; and note that you need to install jupyter console for this enviromnet with 
-  ;; conda install -c anaconda jupyter_console
-  (remove-hook 'elpy-modules 'elpy-module-flymake)
-  (setq python-shell-interpreter "jupyter"
-	python-shell-interpreter-args "console --simple-prompt"
-	       python-shell-prompt-detect-failure-warning nil)
-	 (add-to-list 'python-shell-completion-native-disabled-interpreters "jupyter")
-	 (setq elpy-shell-echo-output nil);; this command is used for fixing ^G problem in MacOS
-	 )
+;; Elpy was tried and deprecated because it becomes lagging with long code files and
+;; data science project with large data tables
+;;
+;; (use-package elpy
+;;   :ensure t
+;;   :after python  
+;;   :config (elpy-enable)
+;;   (setq elpy-rpc-backend "jedi")  
+;;   (setq company-idle-delay 0.5)
+;;   (setq elpy-company-add-completion-from-shell nil)
+;;   ;;(add-hook 'python-mode-hook (lambda () (auto-complete-mode -1)))
+;;   ;;diable auto-complete-mode since it slows down editing and company is the default dependence
+;;   (pyvenv-activate (expand-file-name "~/opt/anaconda3/envs/py38"))
+;;   ;; and note that you need to install jupyter console for this enviromnet with 
+;;   ;; conda install -c anaconda jupyter_console
+;;   (remove-hook 'elpy-modules 'elpy-module-flymake)
+;;   (setq python-shell-interpreter "jupyter"
+;; 	python-shell-interpreter-args "console --simple-prompt"
+;; 	       python-shell-prompt-detect-failure-warning nil)
+;; 	 (add-to-list 'python-shell-completion-native-disabled-interpreters "jupyter")
+;; 	 (setq elpy-shell-echo-output nil);; this command is used for fixing ^G problem in MacOS
+;; 	 )
 ;;Note after install elpy, do not remove the custom-set-varaibles added by Emacs in the end
 
+;; use company mode for auto completion in Python
+;; see https://gist.github.com/yiufung/d8216038252f0488198e8b6af1e2ece4
+(use-package company
+ :ensure t
+ :config
+ (setq company-idle-delay 0
+       company-minimum-prefix-length 2
+       company-show-numbers t
+       company-tooltip-limit 10
+       company-tooltip-align-annotations t
+       ;; invert the navigation direction if the the completion popup-isearch-match
+       ;; is displayed on top (happens near the bottom of windows)
+       company-tooltip-flip-when-above t)
+ (global-company-mode t)
+ )
 
+;; current use anaconda-mode for Python script mode
+(use-package anaconda-mode
+  :ensure t
+  :after python
+  :config
+  (add-hook 'python-mode-hook 'anaconda-mode)
+  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+  (setq python-shell-interpreter "ipython")
+  ;;(add-hook 'python-mode-hook 'company-mode)
+  )
+(use-package company-anaconda
+  :ensure t
+  :init (require 'rx)
+  :after (company)
+  :config
+  (add-to-list 'company-backends 'company-anaconda)
+  )
+;;make sure you install jedi and epc, e.g., conda/pip install jedi epc, in your current enviroment
+
+;; Looks like the below code is not needed, but keep it for now
+;;
+;; If you really want to try with jedi+company, use below scripts it
+;; (remove the :disabled tag)
+;; (use-package jedi
+;;   :disabled
+;;   :after (epc pos-tip)
+;;   :init
+;;   (add-hook 'python-mode-hook 'jedi:setup)
+;;   (add-hook 'python-mode-hook 'jedi:ac-setup)
+;;   :config
+;;   ;; For setup
+;;   ;; http://d.hatena.ne.jp/n-channel/20131220/1387551080
+;;   ;; and this:
+;;   ;; http://kenbell.hatenablog.com/entry/2017/01/15/112344
+
+;;   ;; Under windows, process might very long and EPC may fail.
+;;   ;; Set it larger. What a bummer...
+;;   ;;(if (memq system-type '(ms-dos windows-nt))
+;;   ;;(setq epc:accept-process-timeout 1000))
+;;   )
+;; (use-package company-jedi
+;;   :disabled
+;;   :ensure t
+;;   :config)
+
+
+;; Implementing repl buffer with eval-in-repl, to have Ctrl-Enter to send code to buffer
+;;
+;; Still have issues 1. Code sent to reply have duplicated last char 2. Code region are not
+;; copied to reply
+;;
+;; TODO: fixing issue 1&2
+;; TODO: try jupyter mode to check implementing a VSCode style IDE pattern
+(use-package eval-in-repl
+  :config
+  (setq eir-repl-placement 'right)
+  (require 'eval-in-repl-python)
+  (add-hook 'python-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "<C-return>") 'eir-eval-in-python))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    Emacs IPython Notebook
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package ein
   :defer t
   :config (require 'ein)
@@ -235,6 +349,12 @@
 
 (use-package markdown-mode)  ;required by EIN
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    Orgmode
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package org
   :defer t
   :init
@@ -280,21 +400,17 @@
 (add-hook 'markdown-mode-hook 'auto-fill-mode)
 (add-hook 'markdown-mode-hook 'flyspell-mode)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    Scala mode
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package scala-mode
   :interpreter
   ("scala" . scala-mode))
 
 
-(use-package yasnippet
-  :config (yas-reload-all)
-  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-  (add-hook 'python-mode 'yas-minor-mode)
-  ;;(add-hook 'LaTeX-mode-hook 'yas-minor-mode)
-  (add-hook 'org-mode-hook 'yas-minor-mode)
-  (add-hook 'markdown-mode-hook 'yas-minor-mode)
-  (add-hook 'scala-mode-hook 'yas-minor-mode))
-;; note the snippets bundle needs to be installed separately
-;; use M-x package-list-packages to list all packages available and install yasnippet-snippets or yasnippet-classic-snippets
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -306,7 +422,7 @@
  '(elpy-modules
    '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults))
  '(package-selected-packages
-   '(good-scroll good-scroll-mode smooth-scrolling smooth-scroll yasnippet-snippets window-numbering use-package smart-mode-line scala-mode poly-R neotree miniedit magit htmlize hc-zenburn-theme exec-path-from-shell ess ein centaur-tabs auto-compile auctex all-the-icons))
+   '(yasnippet-classic-snippets eval-in-repl-python eval-in-repl company-jedi jedi company-anaconda good-scroll good-scroll-mode smooth-scrolling smooth-scroll yasnippet-snippets window-numbering use-package smart-mode-line scala-mode poly-R neotree miniedit magit htmlize hc-zenburn-theme exec-path-from-shell ess ein centaur-tabs auto-compile auctex all-the-icons))
  '(pixel-scroll-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
