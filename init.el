78cc78763690bf6dff09e253eb87d48a041ad0be
Guangwei Weng <wgw@WGW-M1Pro.local>
2022-03-13 13:53:12 -0500

    cofig snippet for auctex

--- init.el
+++ init.el
@@ -131,7 +131,7 @@
   (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
   (add-hook 'python-mode-hook 'yas-minor-mode)
   (add-hook 'ess-mode-hook 'yas-minor-mode)
-  ;;(add-hook 'LaTeX-mode-hook 'yas-minor-mode)
+  (add-hook 'LaTeX-mode-hook 'yas-minor-mode)
   (add-hook 'org-mode-hook 'yas-minor-mode)
   (add-hook 'markdown-mode-hook 'yas-minor-mode)
   (add-hook 'scala-mode-hook 'yas-minor-mode)
